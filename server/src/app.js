0126d70e4b3f2f61bf1c59a0dd8d549355bbf72a
m0ohannad <mr.m0ohannad@gmail.com>
2021-03-11 08:55:09 +0300

    heroku

--- server/src/app.js
+++ server/src/app.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/user', router);
+app.use('/user', routes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../client/build')));
