c69a58ce659f341b6b73c386c443f6b42f2a34f6
m0ohannad <mr.m0ohannad@gmail.com>
2021-03-11 09:33:12 +0300

    heroku

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 // const express = require('express');
 // const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
--- server/src/app.js
+++ server/src/app.js
@@ -25,14 +25,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/user', routes);
 
-// if (process.env.NODE_ENV === 'production') {
-app.use(express.static(path.join(__dirname, '../../client/build')));
-app.get('*', (req, res) => {
-    res.sendFile(
-        path.resolve(__dirname, '../../client', 'build', 'index.html')
-    );
-});
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, '../../client', 'build', 'index.html')
+        );
+    });
+}
 
 app.use((req, res, next) => {
     //404 Not Found
