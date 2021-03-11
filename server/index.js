8a3a526c62e40dee2a23dff286c0c7346470bb9c
m0ohannad <mr.m0ohannad@gmail.com>
2021-03-11 11:40:01 +0300

    heroku

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+require('dotenv').config();
 // const express = require('express');
 // const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const joi = require('joi');
+const { hashPassword } = require('../helper');
 const UserModel = require('../models/User');
 
-const userController = require('../controllers/controller');
+// const userController = require('../controllers/controller');
 
 const checkToken = () => {
     return async (req, res, next) => {
@@ -36,24 +37,89 @@ const checkToken = () => {
     };
 };
 
-router.post('/register', function (req, res, next) {
-    userController.regisetr
+router.post('/register', async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const bodySchhema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(6).required()
+    });
+
+    const validationResult = bodySchhema.validate(req.body);
+    if (validationResult.error) {
+        res.statusCode = 400;
+        res.send({ message: validationResult.error.details[0].message });
+        return;
+    }
+
+    try {
+        const newUser = new UserModel({
+            name,
+            email,
+            password
+        });
+
+        await newUser.save();
+        const user = await UserModel.findOne({ email });
+        const expire = process.env.JWT_EXPIRATION;
+        const token = jwt.sign({ sub: user._id }, user.salt, { expiresIn: expire });
+        res.send({ token: token })
+
+    } catch (e) {
+        if (e.code === 11000 && e.name === 'MongoError') {
+            const error = new Error(`Email address ${email} is already taken`);
+            error.status = 400
+            next(error);
+        } else {
+            next(e);
+        }
+    }
 });
 
-router.post('/login', function (req, res, next) {
-    userController.login
+router.post('/login', async (req, res, next) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    const expire = process.env.JWT_EXPIRATION;
+    try {
+        if (!user) {
+            const err = new Error(`No User found with Email ${email}`);
+            err.status = 401;
+            next(err);
+        }
+
+        if (user.password === hashPassword(password, user.salt)) {
+            const token = jwt.sign({ sub: user._id }, user.salt, { expiresIn: expire });
+            res.send({ token: token })
+        } else {
+            res.statusCode = 401;
+            res.send({ message: 'Password is Wrong!' });
+        }
+    } catch (e) {
+        next(e);
+    }
 });
 
-router.get('/profile', function (req, res, next) {
-    userController.profile
+router.get('/profile', (req, res, next) => {
+    const user = req.user
+    if (!user) {
+        res.statusCode = 404;
+        res.send({ message: 'User with this ID does not exist!' });
+    } else res.send({ user: user })
 });
 
-router.put('/', checkToken(), function (req, res) {
-    userController.edit
+router.put('/', checkToken(), async (req, res) => {
+    UserModel.findByIdAndUpdate(req.user._id, { $set: req.body }, { new: true }, function (err, result) {
+        if (err) {
+            res.send({ message: err });
+        }
+        res.send({ user: result })
+    });
 });
 
-router.delete('/:id', checkToken(), function (req, res) {
-    userController.delete
+router.delete('/:id', checkToken(), (req, res) => {
+    UserModel.findOneAndDelete({ "_id": req.params.id })
+        .then(data => res.json(data))
+        .catch(next)
 });
 
 router.get('*', (req, res) => res.send('URL not found!MMM Mohannad'));
